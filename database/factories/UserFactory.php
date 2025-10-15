548194a7f27cb6979013abba801bf1bc8d894e59
techabdu <maiganye33@gmail.com>
2025-10-15 07:00:36 +0100

    fix: Correct UserFactory namespace for seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use function Illuminate\Support\Testing\Faker\fake;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>

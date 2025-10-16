8b9fe9492819ed91e795b6127072bcc9d580b22c
techabdu <maiganye33@gmail.com>
2025-10-16 07:42:51 +0100

    fix: Update central user migration and remove obsolete migration

--- database/migrations/0001_01_01_000000_create_central_users_table.php
+++ database/migrations/0001_01_01_000000_create_central_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('role_id')->nullable()->constrained('roles')->onDelete('set null');
             $table->timestamps();
         });
 
